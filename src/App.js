595fb960a0e0ed33d2238d40feebc9fbd1f562a2
SumanthBHegde <sumanthbhegde12@gmail.com>
2024-09-19 22:49:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import States from "./data.json";
+import { Map } from "react-map-gl";
+import DeckGL, { GeoJsonLayer } from "deck.gl";
+
+// Access the MAPBOX_ACCESS_TOKEN environment variable
+const MAPBOX_ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+const MAP_STYLE =
+  "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json";
+
+const INITIAL_VIEW_STATE = {
+  latitude: 12.977439,
+  longitude: 77.570839,
+  zoom: 3,
+  bearing: 0,
+  pitch: 30,
+};
 
 function App() {
+  const onClick = (info) => {
+    if (info.object) {
+      alert(info.object.properties.st_nm);
+    }
+  };
+
+  const layers = [
+    new GeoJsonLayer({
+      id: "States",
+      data: States,
+      //styles
+      filled: true,
+      pointRadiusMinPixels: 5,
+      pointRadiusScale: 2000,
+      getFillColor: [86, 144, 58, 250],
+      pickable: true,
+      autoHighlight: true,
+      onClick,
+    }),
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DeckGL
+      initialViewState={INITIAL_VIEW_STATE}
+      controller={true}
+      layers={layers}
+    >
+      <Map mapStyle={MAP_STYLE} mapboxAccessToken={MAPBOX_ACCESS_TOKEN} />
+    </DeckGL>
   );
 }
 
